38dfd89b5482079ebdb673a45415583002d76814
sofiamartinezparada <55553291+sofiamartinezparada@users.noreply.github.com>
2021-04-07 11:47:04 +0200

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -15,7 +15,7 @@ CREATE TABLE  IF NOT EXISTS `usuarios`(
 CREATE TABLE   IF NOT EXISTS `idiomas_usuario`(
   `user_ID` int (11) NOT NULL,
   `idioma` varchar (100) NOT NULL,
-  PRIMARY KEY (`user_ID`),
+
   CONSTRAINT `user_ID` FOREIGN KEY (`user_ID`) REFERENCES `usuarios`(`user_ID`)
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -35,11 +35,28 @@ CREATE TABLE  IF NOT EXISTS `trabajos`(
   `descripcion` varchar(100) NOT NULL,
   `numero_vacantes`  int(11),
   `trabajo_ID` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `lugar` varchar(100) NOT NULL,
+  `lugar` varchar(100) ,
+  `empresa` varchar(100) NOT NULL,
   `salario`  int(8) NOT NULL,
   `link`  varchar(100) NOT NULL,
   `estudio_minimos`  varchar(100) NOT NULL,
   `experiencia_minima`  varchar(100) NOT NULL,
   `horario` varchar(100) NOT NULL,
   `beneficios_sociales`varchar(100) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE   IF NOT EXISTS `Users_Viviendas`(
+  `user_ID` int (11) NOT NULL,
+  `vivienda_ID` varchar (100) NOT NULL,
+  CONSTRAINT `user_ID` FOREIGN KEY (`user_ID`) REFERENCES `usuarios`(`user_ID`),
+  CONSTRAINT `vivienda_ID` FOREIGN KEY (`vivienda_ID`) REFERENCES `usuarios`(`vivienda_ID`)
+
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE   IF NOT EXISTS `Users_Trabajos`(
+  `user_ID` int (11) NOT NULL,
+  `trabajo_ID` varchar (100) NOT NULL,
+  CONSTRAINT `user_ID` FOREIGN KEY (`user_ID`) REFERENCES `usuarios`(`user_ID`),
+  CONSTRAINT `trabajo_ID` FOREIGN KEY (`trabajo_ID`) REFERENCES `usuarios`(`trabajo_ID`)
+
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
