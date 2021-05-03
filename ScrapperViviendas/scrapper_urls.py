a753e73fd813893ae3665aa582aa9dae41eb1fba
sofiamartinezparada <55553291+sofiamartinezparada@users.noreply.github.com>
2021-05-03 10:49:10 +0200

    actualizacion json

--- ScrapperViviendas/scrapper_urls.py
+++ ScrapperViviendas/scrapper_urls.py
@@ -14,31 +14,30 @@ for i in range (2, 15):
         try:
                 page = requests.get(url_dinamica)
                 if(page.status_code == 200):
-                        if(page != None):
-                                soup = ' '
-                                soup = BeautifulSoup(page.content, 'html.parser')
-                                section = ''
-                                #Comprueba que existan los campos que se necesitan
-                                if(str(soup).find('re-Card-primary') & str(soup).find('re-Searchresult-itemRow')):
-                                        section = soup.find('section', class_ = 're-Searchresult')
-                                        div_fuera = ' '
-                                        div_fuera = section.findAll('article', class_ = 're-Searchresult-itemRow')
-                                        #Al ser una pagina dinamica estamos perdiendo todas las ofertas de debajo que no estan cargadas
-                                        #así que para que sea mas rapido se limita esta busqueda
-                                        for p in range (0, 4):
-                                                div = div_fuera[p]
-                                                div_interesante = ' '
-                                                div_interesante = div.find('div', class_ = 're-Card-primary')
-                                                if (div_interesante != None):  
-                                                        a = div_interesante.find('a', href=True)
-                                                        if(a['href'].find(',') == -1):
-                                                                link = 'https://www.fotocasa.es' + a['href']
-                                                                outF = open("./nuevos.txt", "a")
-                                                                outF.write(link)
-                                                                outF.write(',')
-                                                                outF.close()
-                                        comandosleep = 'sleep ' + str(randrange(3))
-                                        os.system(comandosleep)
+                        soup = ' '
+                        soup = BeautifulSoup(page.content, 'html.parser')
+                        section = ''
+                        #Comprueba que existan los campos que se necesitan
+                        if(str(soup).find('re-Card-primary') & str(soup).find('re-Searchresult-itemRow')):
+                                section = soup.find('section', class_ = 're-Searchresult')
+                                div_fuera = ' '
+                                div_fuera = section.findAll('article', class_ = 're-Searchresult-itemRow')
+                                #Al ser una pagina dinamica estamos perdiendo todas las ofertas de debajo que no estan cargadas
+                                #así que para que sea mas rapido se limita esta busqueda
+                                for p in range (0, 4):
+                                        div = div_fuera[p]
+                                        div_interesante = ' '
+                                        div_interesante = div.find('div', class_ = 're-Card-primary')
+                                        if (div_interesante != None):  
+                                                a = div_interesante.find('a', href=True)
+                                                if(a['href'].find(',') == -1):
+                                                        link = 'https://www.fotocasa.es' + a['href']
+                                                        outF = open("./nuevos.txt", "a")
+                                                        outF.write(link)
+                                                        outF.write(',')
+                                                        outF.close()
+                                comandosleep = 'sleep ' + str(randrange(3))
+                                os.system(comandosleep)
                 else:
                         comandosleep = 'sleep ' + str(randrange(3))
                         os.system(comandosleep)
