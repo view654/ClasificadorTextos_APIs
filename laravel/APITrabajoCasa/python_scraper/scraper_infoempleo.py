0bc4f384324b3a70f4b2a146b09abda48f6b422d
Claudia Jazmín Soria Saavedra <clxudiajazmin@gmail.com>
2021-05-05 14:25:53 +0200

    Actualizo Python Scraper y JSON

--- laravel/APITrabajoCasa/python_scraper/scraper_infoempleo.py
+++ laravel/APITrabajoCasa/python_scraper/scraper_infoempleo.py
@@ -1,4 +1,6 @@
 import json
+from bs4 import BeautifulSoup
+import requests, re, datetime, time, threading
 
 urls = ['https://www.infoempleo.com/trabajo/en_a-corunna/', 
 'https://www.infoempleo.com/trabajo/en_alava/', 
@@ -53,11 +55,31 @@ urls = ['https://www.infoempleo.com/trabajo/en_a-corunna/',
 'https://www.infoempleo.com/trabajo/en_zamora/',
 'https://www.infoempleo.com/trabajo/en_zaragoza/']
 
+#Información de las ofertas de trabajo
 ofertas_trabajo = []
+#Enlaces de las ofertas de trabajo antiguas y nuevas
+links_trabajos = []
+#Enlaces únicos, sin repetidos
+ofertas_trabajo_unicas = []
 
-from bs4 import BeautifulSoup
-import requests, re, csv, datetime, time, threading, random
+#Se guardan toda la info del JSON anterior de ofertas de trabajo
+with open('laravel\APITrabajoCasa\data_json\ofertas_trabajo.json', 'r') as f:
+    trabajos_dict = json.load(f)
 
+#Se recorre la información guardando únicamente los enlaces
+for trabajo in trabajos_dict:
+        links_trabajos.append(trabajo['enlace'])
+
+#Se ingresa a cada enlace para validar la existencia de la oferta de trabajo, 
+#de forma que se elimine o almacene nuevamente
+for link in links_trabajos:
+    oferta = requests.get(link)
+    soup1 = BeautifulSoup(oferta.content, 'html.parser')
+    pagina = soup1.find('div', class_ = 'top-bar oferta-inactiva offer-detail')
+    if(pagina):
+        links_trabajos.remove(link)
+
+#Se agregan nuevas ofertas de las páginas de infoempleo
 for url in urls:
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -70,37 +92,46 @@ for url in urls:
     for oferta in ofertas:
         titulo = oferta.find('h2', class_ = 'title col-xs-12 col-sm-9 col-md-9')
         link = titulo.find('a')
+
         #LINK OFERTA
         link_oferta = "https://www.infoempleo.com" + link['href']
-        ioferta = requests.get(link_oferta)
-        soup1 = BeautifulSoup(ioferta.content, 'html.parser')
 
-        #TITULO DE OFERTA
-        titulo_oferta = soup1.find('h1', class_ = 'h1 regular')
-        '''
-        #COMPAÑIA DE OFERTA
-        div1 = soup1.find('div', class_ = 'company')
-        compania = div1.find('h2', class_ = 'title')
-        print(compania)
-        '''
+        links_trabajos.append(link_oferta)
 
-        #LOCALIDAD DE OFERTA
-        ul = soup1.find_all('ul', class_ = 'details inline')
-        li = ul[1].find('li')
-        localidad = li.get_text()
+#Se eliminan las duplicadas del arreglo de enlaces
+for link in links_trabajos:
+    if link not in ofertas_trabajo_unicas:
+        ofertas_trabajo_unicas.append(link)
 
-        #DETALLES DE OFERTA
-        p = soup1.find('p', class_ = 'small mt10')
-        detalles = p.get_text().split('-')
-        jornada = detalles[0]
-        contrato = detalles[1]
-        salario = detalles[2]
-        experiencia = detalles[3]
+#Se realiza el scrapper de cada una de las ofertas por enlace
+for link in ofertas_trabajo_unicas:
+    ioferta = requests.get(link)
+    soup1 = BeautifulSoup(ioferta.content, 'html.parser')
 
-        offer = soup1.find('div', class_ = 'offer')
-        if(offer):
-            pre = offer.find_all('pre')
+    #TITULO DE OFERTA
+    titulo_oferta = soup1.find('h1', class_ = 'h1 regular')
 
+    #LOCALIDAD DE OFERTA
+    ul = soup1.find_all('ul', class_ = 'details inline')
+    li = ul[1].find('li')
+    localidad = li.get_text()
+
+    #DETALLES DE OFERTA
+    p = soup1.find('p', class_ = 'small mt10')
+    detalles = p.get_text().split('-')
+    jornada = detalles[0]
+    contrato = detalles[1]
+    salario = detalles[2]
+    experiencia = detalles[3]
+
+    funciones = ''
+    requisitos = ''
+    ofrece = ''
+    area = ''
+
+    offer = soup1.find('div', class_ = 'offer')
+    if(offer):
+        pre = offer.find_all('pre')
         if(len(pre) > 2):
             funciones = pre[0].get_text()
             requisitos = pre[1].get_text()
@@ -110,46 +141,28 @@ for url in urls:
             requisitos = pre[1].get_text()
             ofrece = ''
 
-        #AREA
-        ul = soup1.find('ul', class_ = 'inline boxes mt30')
-        if(ul):
-            li = ul.find_all('li')
-            area = li[0].find('p').get_text()
-            vacantes = li[2].find('p').get_text()
+    #AREA
+    ul = soup1.find('ul', class_ = 'inline boxes mt30')
+    if(ul):
+        li = ul.find_all('li')
+        area = li[0].find('p').get_text()
         
-        oferta_trabajo = {
-            "titulo": titulo_oferta.get_text(),
-            "enlace": link_oferta,
-            "jornada": jornada,
-            "contrato": contrato,
-            "salario": salario,
-            "experiencia": experiencia,
-            "funciones": funciones,
-            "requisitos": requisitos,
-            "ofrece": ofrece,
-            "area": area,
-            "localidad": localidad
-        }
+    oferta_trabajo = {
+        "titulo": titulo_oferta.get_text(),
+        "enlace": link,
+        "jornada": jornada,
+        "contrato": contrato,
+        "salario": salario,
+        "experiencia": experiencia,
+        "funciones": funciones,
+        "requisitos": requisitos,
+        "ofrece": ofrece,
+        "area": area,
+        "localidad": localidad
+    }
 
-        print(titulo_oferta.get_text())
-        print(link_oferta)
-        
-        print(localidad, "\n")
-        '''
-        
-        print(jornada)
-        print(contrato)
-        print(salario)
-        print(experiencia)
-        print(funciones)
-        print(requisitos)
-        print(ofrece)
-        print(area)
-        '''
-        ofertas_trabajo.append(oferta_trabajo)
+    ofertas_trabajo.append(oferta_trabajo)
 
-
-
-with open('ofertas_trabajo', 'w') as json_file:
+#Se almacena la información en formato JSON en el archivo especificado
+with open('laravel\APITrabajoCasa\data_json\ofertas_trabajo.json', 'w') as json_file:
     json.dump(ofertas_trabajo, json_file)
-
