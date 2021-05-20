10ecd4c38a950a198a4a8cfa5218adb762c4b258
Claudia Jazmín Soria Saavedra <clxudiajazmin@gmail.com>
2021-05-20 10:08:22 +0200

    Actualizo código

--- laravel/APITrabajoCasa/app/Http/Controllers/ViviendasController.php
+++ laravel/APITrabajoCasa/app/Http/Controllers/ViviendasController.php
@@ -19,11 +19,6 @@ class ViviendasController extends Controller
         return $json;
     }
 
-
-    public function mostrarTodasViviendas(){
-        return Vivienda::all();
-    }
-
     //Viviendas en BBDD
     public function mostrarTodasViviendas(){
         return Vivienda::all();
@@ -83,106 +78,5 @@ class ViviendasController extends Controller
         return response() -> json($filtrado);
     }
 
-    //General
-    public function filtroGeneral($lugar, $preciomax, $preciomin, $habitacionesmax , $habitacionesmin, $banosmax , $banosmin, $metros2max, $metros2min, $planta, $compr_alq_compar, $tipo){
-
-        $path = '../python_scraper/ofertas_vivienda.json';
-        $json = file_get_contents($path);
-        $array = json_decode($json);
-
-        if (!empty($lugar)){
-            $array = array_filter($array, function($val) use ($lugar){
-
-                return $val -> lugar == $lugar;
-
-            });
-        }
-
-        if (!empty($preciomax)){
-            $array = array_filter($array, function($val) use ($preciomax){
-
-                return $val -> precio <= $preciomax;
-
-            });
-        }
-
-        if (!empty($preciomin)){
-            $array = array_filter($array, function($val) use ($preciomin){
-
-                return $val -> precio >= $preciomin;
-
-            });
-        }
-
-        if (!empty($habitacionesmax)){
-            $array = array_filter($array, function($val) use ($habitacionesmax){
-
-                return $val -> habitaciones <= $habitacionesmax;
-
-            });
-        }
-        if (!empty($habitacionesmin)){
-            $array = array_filter($array, function($val) use ($habitacionesmax){
-
-                return $val -> habitaciones >= $habitacionesmin;
-
-            });
-        }
-
-        if (!empty($banosmax)){
-            $array = array_filter($array, function($val) use ($banosmax){
-
-                return $val -> banos <= $banosmax;
-
-            });
-        }
-        if (!empty($banosmin)){
-            $array = array_filter($array, function($val) use ($banosmin){
-
-                return $val -> banos >= $banosmin;
-
-            });
-        }
-
-        if (!empty($metros2max)){
-            $array = array_filter($array, function($val) use ($metros2max){
-
-                return $val -> metros2 <= $metros2max;
-
-            });
-        }
-        if (!empty($metros2min)){
-            $array = array_filter($array, function($val) use ($metros2min){
-
-                return $val -> metros2 >= $metros2min;
-
-            });
-        }
-
-        if (!empty($planta)){
-            $array = array_filter($array, function($val) use ($planta){
-
-                return $val -> planta == $planta;
-
-            });
-        }
-
-        if (!empty($compr_alq_compar)){
-            $array = array_filter($array, function($val) use ($compr_alq_compar){
-
-                return $val -> compr_alq_compar == $compr_alq_compar;
-
-            });
-        }
-
-        if (!empty($tipo)){
-            $array = array_filter($array, function($val) use ($tipo){
-
-                return $val -> tipo == $tipo;
-
-            });
-        }
-        return response() -> json($array);
-    }
 
 }
