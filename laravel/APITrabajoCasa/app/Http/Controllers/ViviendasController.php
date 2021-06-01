cfeafa97be1911ca3762e7a0eb72503d9178bfe7
Claudia Jazmín Soria Saavedra <clxudiajazmin@gmail.com>
2021-06-01 10:35:58 +0200

    Buscador Filtro Vivienda funcionando

--- laravel/APITrabajoCasa/app/Http/Controllers/ViviendasController.php
+++ laravel/APITrabajoCasa/app/Http/Controllers/ViviendasController.php
@@ -25,24 +25,217 @@ class ViviendasController extends Controller
         return Vivienda::all();
     }
 
-    public function filtroBusquedaVivienda($request = null){
+    public function filtroBusquedaVivienda($request = null, $lugar = null, $preciomax= null, $preciomin= null, $habitacionesmax = null, $habitacionesmin= null, $banosmax = null, $banosmin= null, $metros2max= null, $metros2min= null, $planta= null, $compr_alq_compar= null, $tipo= null){
         $path = '../python_scraper/ofertas_viviendas.json';
         $json = file_get_contents($path);
         $array = json_decode($json);
+        $filtro = '';
 
         if(strcmp($request, "null")===0){
-            $filtrado = $array;
+            $filtro = $json;
+
+            if (strcmp($lugar, "null")===0){
+
+                $filtro = json_decode($json);
+            }else{
+    
+                $filtro = array_filter($array, function($val) use ($lugar){
+                    return $val -> lugar == $lugar;});
+    
+            }
+            if (strcmp($preciomax, "null")===0){
+    
+            }else{
+                $filtro = array_filter($filtro, function($val) use ($preciomax){
+                    return $val -> precio <= (int)$preciomax;
+    
+                });
+            }
+    
+            if (strcmp($preciomin, "null")===0){
+            }else{
+                $filtro = array_filter($filtro, function($val) use ($preciomin){
+                    return $val -> precio >= (int)$preciomin;
+                });
+            }
+    
+            if (strcmp($habitacionesmax, "null")===0){
+            }else{
+                $filtro = array_filter($filtro, function($val) use ($habitacionesmax){
+                    return $val -> habitaciones <= (int)$habitacionesmax;
+                });
+            }
+            if (strcmp($habitacionesmin, "null")===0){}
+            else{
+                echo($habitacionesmin);
+                $filtro = array_filter($filtro, function($val) use ($habitacionesmin){
+                    return $val -> habitaciones >= (int)$habitacionesmin;
+                });
+            }
+    
+            if (strcmp($banosmax, "null")===0){}
+            else{
+                $filtro = array_filter($filtro, function($val) use ($banosmax){
+    
+                    return $val -> banos <= (int)$banosmax;
+    
+                });
+            }
+            if (strcmp($banosmin, "null")===0){}
+            else{
+                $filtro = array_filter($filtro, function($val) use ($banosmin){
+                    return $val -> banos >= (int)$banosmin;
+    
+                });
+            }
+    
+            if (strcmp($metros2max, "null")===0){}
+            else{
+                $filtro = array_filter($filtro, function($val) use ($metros2max){
+                    return $val -> metros2 <= (int)$metros2max;
+    
+                });
+            }
+            if (strcmp($metros2min, "null")===0){}
+            else{
+                $filtro = array_filter($filtro, function($val) use ($metros2min){
+                    return $val -> metros2 >= (int)$metros2min;
+    
+                });
+            }
+    
+            if (strcmp($planta, "null")===0){}
+            else{
+                $filtro = array_filter($filtro, function($val) use ($planta){
+                    return $val -> planta == (int)$planta;
+    
+                });
+            }
+    
+            if (strcmp($compr_alq_compar, "null")===0){}
+            else{
+                $filtro = array_filter($filtro, function($val) use ($compr_alq_compar){
+    
+                    return $val -> compr_alq_compar == $compr_alq_compar;
+    
+                });
+            }
+    
+            if (strcmp($tipo, "null")===0){}
+            else{
+                $filtro = array_filter($filtro, function($val) use ($tipo){
+    
+                    return $val -> tipo == $tipo;
+    
+                });
+            }
         }else{
 
-        $filtrado = array_filter($array, function($val) use ($request) { 
-            $comprobar = Str::contains(strtolower($val -> lugar), strtolower($request));
-            if ($comprobar) {
-                return $val -> lugar;
+            $filtrado = array_filter($array, function($val) use ($request) { 
+                $comprobar = Str::contains(strtolower($val -> lugar), strtolower($request));
+                if ($comprobar) {
+                    return $val -> lugar;
+                }
+                
+            });
+
+            if (strcmp($lugar, "null")===0){
+
+                $filtro = $filtrado;
+            }else{
+    
+                $filtro = array_filter($array, function($val) use ($lugar){
+                    return $val -> lugar == $lugar;});
+    
             }
-            
-        });
+            if (strcmp($preciomax, "null")===0){
+    
+            }else{
+                $filtro = array_filter($filtro, function($val) use ($preciomax){
+                    return $val -> precio <= (int)$preciomax;
+    
+                });
+            }
+    
+            if (strcmp($preciomin, "null")===0){
+            }else{
+                $filtro = array_filter($filtro, function($val) use ($preciomin){
+                    return $val -> precio >= (int)$preciomin;
+                });
+            }
+    
+            if (strcmp($habitacionesmax, "null")===0){
+            }else{
+                $filtro = array_filter($filtro, function($val) use ($habitacionesmax){
+                    return $val -> habitaciones <= (int)$habitacionesmax;
+                });
+            }
+            if (strcmp($habitacionesmin, "null")===0){}
+            else{
+                echo($habitacionesmin);
+                $filtro = array_filter($filtro, function($val) use ($habitacionesmin){
+                    return $val -> habitaciones >= (int)$habitacionesmin;
+                });
+            }
+    
+            if (strcmp($banosmax, "null")===0){}
+            else{
+                $filtro = array_filter($filtro, function($val) use ($banosmax){
+    
+                    return $val -> banos <= (int)$banosmax;
+    
+                });
+            }
+            if (strcmp($banosmin, "null")===0){}
+            else{
+                $filtro = array_filter($filtro, function($val) use ($banosmin){
+                    return $val -> banos >= (int)$banosmin;
+    
+                });
+            }
+    
+            if (strcmp($metros2max, "null")===0){}
+            else{
+                $filtro = array_filter($filtro, function($val) use ($metros2max){
+                    return $val -> metros2 <= (int)$metros2max;
+    
+                });
+            }
+            if (strcmp($metros2min, "null")===0){}
+            else{
+                $filtro = array_filter($filtro, function($val) use ($metros2min){
+                    return $val -> metros2 >= (int)$metros2min;
+    
+                });
+            }
+    
+            if (strcmp($planta, "null")===0){}
+            else{
+                $filtro = array_filter($filtro, function($val) use ($planta){
+                    return $val -> planta == (int)$planta;
+    
+                });
+            }
+    
+            if (strcmp($compr_alq_compar, "null")===0){}
+            else{
+                $filtro = array_filter($filtro, function($val) use ($compr_alq_compar){
+    
+                    return $val -> compr_alq_compar == $compr_alq_compar;
+    
+                });
+            }
+    
+            if (strcmp($tipo, "null")===0){}
+            else{
+                $filtro = array_filter($filtro, function($val) use ($tipo){
+    
+                    return $val -> tipo == $tipo;
+    
+                });
+            }
         }
-        return response() -> json($filtrado); 
+        return response() -> json($filtro); 
 
     }
   
